d95f4ea30e96e32627601cde5a0d8cb3125c61ee
serjG901 <70537823+serjG901@users.noreply.github.com>
2020-11-16 21:58:44 +0300

    add style shadow-md in MyError

--- src/MyError.js
+++ src/MyError.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function MyError({ children }) {
   return (
-    <div className="absolute w-1/2 bg-white text-red-600 rounded">{children}</div>
+    <div className="absolute w-1/2 shadow-md bg-white text-red-600 rounded">{children}</div>
   );
 }
