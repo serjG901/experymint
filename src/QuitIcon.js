27b9141792938379969e584e0efc2dd2cee00e2d
serjG901 <70537823+serjG901@users.noreply.github.com>
2020-11-17 16:57:02 +0300

    Update QuitIcon.js

--- src/QuitIcon.js
+++ src/QuitIcon.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function QuitIcon() {
   return (
     <svg className="inline-block" height="36" width="36">
-      <path d="M8 28 L28 8" stroke="white" stroke-width="4" />
-      <path d="M8 8 L28 28" stroke="white" stroke-width="4" />
+      <path d="M10 26 L26 10" stroke="white" stroke-width="4" />
+      <path d="M10 10 L26 26" stroke="white" stroke-width="4" />
     </svg>
   );
 }
