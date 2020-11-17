f0b84fcfec50c951aee7c7b92d95231c911db682
serjG901 <70537823+serjG901@users.noreply.github.com>
2020-11-17 16:15:30 +0300

    Update TestIcon.js

--- src/TestIcon.js
+++ src/TestIcon.js
@@ -1,24 +1,25 @@
 import React from "react";
 
-export default function TestIcon({ isActive }) {
+export default function TestIcon() {
   return (
-    <svg className="inline-block transform scale-150" width="24" height="24">
-      <circle cx="12" cy="6" r="6" fill="rgba(255,255,0,0.5)" />
-      <circle cx="6" cy="12" r="6" fill="rgba(0,255,0,0.5)" />
-      <circle cx="12" cy="18" r="6" fill="rgba(255,0,0,0.5)" />
-      <circle cx="18" cy="12" r="6" fill="rgba(0,0,255,0.5)" />
-      <circle cx="6" cy="12" r="6" fill="rgba(0,255,0,0.5)" />
-      <circle cx="12" cy="6" r="6" fill="rgba(255,255,0,0.5)" />
-      <circle cx="18" cy="12" r="6" fill="rgba(0,0,255,0.5)" />
-      <circle cx="12" cy="18" r="6" fill="rgba(255,0,0,0.5)" />
+    <svg className="inline-block" width="36" height="36">
+      <circle cx="18" cy="9" r="9" fill="rgba(255,255,0,0.5)" />
+      <circle cx="9" cy="18" r="9" fill="rgba(0,255,0,0.5)" />
+      <circle cx="18" cy="27" r="9" fill="rgba(255,0,0,0.5)" />
+      <circle cx="27" cy="18" r="9" fill="rgba(0,0,255,0.5)" />
+      <circle cx="9" cy="18" r="9" fill="rgba(0,255,0,0.5)" />
+      <circle cx="18" cy="9" r="9" fill="rgba(255,255,0,0.5)" />
+      <circle cx="27" cy="18" r="9" fill="rgba(0,0,255,0.5)" />
+      <circle cx="18" cy="27" r="9" fill="rgba(255,0,0,0.5)" />
       <circle
-        cx="12"
-        cy="12"
-        r="10"
+        cx="18"
+        cy="18"
+        r="15"
         stroke="white"
-        strokeWidth="4"
+        strokeWidth="6"
         fill="rgba(255,255,255,0)"
       />
     </svg>
   );
 }
+
