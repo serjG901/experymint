a8594213ef37ce46f5c548fad84f0f7a272ea8d5
serjG901 <70537823+serjG901@users.noreply.github.com>
2020-11-17 16:50:05 +0300

    add QuitIcon.js

---
+++ src/QuitIcon.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function QuitIcon() {
+  return (
+    <svg className="inline-block" height="36" width="36">
+      <path d="M8 28 L28 8" stroke="white" stroke-width="4" />
+      <path d="M8 8 L28 28" stroke="white" stroke-width="4" />
+    </svg>
+  );
+}
