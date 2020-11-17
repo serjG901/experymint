f46946a6e3d1c0b9bec0794d2885f69c6aa8c9dd
serjG901 <70537823+serjG901@users.noreply.github.com>
2020-11-17 19:07:36 +0300

    Update QuitIcon.js
    change color white -> gray

--- src/QuitIcon.js
+++ src/QuitIcon.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function QuitIcon() {
   return (
     <svg className="inline-block" height="36" width="36">
-      <path d="M10 26 L26 10" stroke="white" strokeWidth="4" />
-      <path d="M10 10 L26 26" stroke="white" strokeWidth="4" />
+      <path d="M10 26 L26 10" stroke="gray" strokeWidth="4" />
+      <path d="M10 10 L26 26" stroke="gray" strokeWidth="4" />
     </svg>
   );
 }
