51c37104ac72220483b0902de47bb2f17b2dd3c0
serjG901 <70537823+serjG901@users.noreply.github.com>
2020-11-26 23:52:45 +0300

    clear code in usersData

--- src/lib/usersData.js
+++ src/lib/usersData.js
@@ -11,10 +11,6 @@ if (localDataUsers.size > 0) {
   }
 }
 
-export function isNameFree(name) {
-  return !dataUsers.has(name);
-}
-
 export function addDataInStorage() {
   let err = false;
   try {
@@ -27,6 +23,18 @@ export function addDataInStorage() {
     err = e;
   }
   if (err) return false;
+  return true;
+}
+
+export function isNameFree(name) {
+  return !dataUsers.has(name);
+}
+
+export function isPasswordCorrect({ name, pass }) {
+  const user = dataUsers.get(name);
+  if (user.pass !== pass) return false;
+  User.currentID = name;
+  console.log(User.currentID);
   return true;
 }
 
@@ -56,83 +64,20 @@ export function setUserData({ name, pass }) {
   }
 }
 
-export function isPasswordCorrect({ name, pass }) {
-  const user = dataUsers.get(name);
-  if (user.pass !== pass) return false;
-  User.currentID = name;
-  console.log(User.currentID);
-  return true;
-}
-
 export function getUserData(userID) {
   const userData = dataUsers.get(userID);
-  return { ...userData, pass: "***" };
-}
-
-export function addResultInUser(results, numberImage, choice) {
-  let newMistruth = false;
-  if (results.hasOwnProperty(numberImage)) {
-    newMistruth = results[numberImage] !== choice;
-    const newResults = {
-      ...results,
-      [numberImage]: choice
-    };
-    return { newResults, newMistruth };
-  }
-  const newResults = { ...results, [numberImage]: choice };
-  return { newResults, newMistruth };
-}
-
-export function updateUserResults(userID, numberImage, choice) {
-  const userData = dataUsers.get(userID);
-  const { newResults, newMistruth } = addResultInUser(
-    userData.results,
-    numberImage,
-    choice
-  );
-  const newUserData = {
-    ...userData,
-    results: newResults,
-    score: userData.score + 1,
-    mistruth: userData.mistruth + newMistruth
+  return {
+    name: userData.name,
+    manifest: userData.manifest,
+    score: userData.score,
+    mistruth: userData.mistruth,
+    avatar: userData.avatar,
+    tags: userData.tags,
+    results: userData.results
   };
-  dataUsers.set(userID, newUserData);
-  const statusSavedData = addDataInStorage();
-  if (statusSavedData) return true;
-  dataUsers.set(userID, userData);
-  return false;
-}
-
-export function getDataForType(userID, typeData) {
-  let outData = {};
-  const filterTags = dataUsers.get(userID).filter
-    ? dataUsers.get(userID).filter.toLowerCase()
-    : "";
-
-  for (let entry of dataUsers) {
-    if (entry[0] !== userID) {
-      if (entry[1]["tags"].toLowerCase().indexOf(filterTags) !== -1)
-        outData = { ...outData, [entry[0]]: entry[1][typeData] };
-    }
-  }
-  return outData;
-}
-
-export function updateUserData(userID, typeData, dataSource) {
-  const userData = dataUsers.get(userID);
-  const newUserData = {
-    ...userData,
-    [typeData]: dataSource
-  };
-  dataUsers.set(userID, newUserData);
-  const statusSavedData = addDataInStorage();
-  if (statusSavedData) return true;
-  dataUsers.set(userID, userData);
-  return false;
 }
 
 export function updateUser(userID, user) {
-  //console.log(userID, user);
   const userData = dataUsers.get(userID);
   const newUserData = {
     ...user,
@@ -145,26 +90,9 @@ export function updateUser(userID, user) {
   return false;
 }
 
-export function getOtherUsersResults(userID, filter) {
-  let otherUsersResults = {};
-  const filterTags = filter ? filter.toLowerCase() : "";
-
-  for (let entry of dataUsers) {
-    if (entry[0] !== userID) {
-      if (entry[1]["tags"].toLowerCase().indexOf(filterTags) !== -1)
-        otherUsersResults = {
-          ...otherUsersResults,
-          [entry[0]]: entry[1].results
-        };
-    }
-  }
-  return otherUsersResults;
-}
-
 export function getOtherUsers(userID, filter) {
   let otherUsersInfo = {};
   const filterTags = filter ? filter.toLowerCase() : "";
-
   for (let entry of dataUsers) {
     if (entry[0] !== userID) {
       if (entry[1]["tags"].toLowerCase().indexOf(filterTags) !== -1)
